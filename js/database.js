72543d3f8848aea44ba7f0ee0c1fac9696bf5e89
IkraT101 <IkraTameel@hotmail.com>
2019-11-30 21:36:24 +0000

    update
    update

--- js/database.js
+++ js/database.js
@@ -1,4 +1,8 @@
-if('serviceWorker' in navigator){
+serviceWorker: {
+            path: '/sw.js',
+          }
+
+  if('serviceWorker' in navigator){
 navigator.serviceWorker.register('/sw.js')
   .then((reg) => console.log('service worker registered', reg))
   .catch((err) => console.log('service worker not registered', err));
