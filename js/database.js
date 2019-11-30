ebfaf8575615f7f2fe34bd904d7e04a911b06c2f
IkraT101 <IkraTameel@hotmail.com>
2019-11-30 22:20:05 +0000

    adding a new data base
    new attempt at server register

--- js/database.js
+++ js/database.js
@@ -1,9 +1,14 @@
-serviceWorker: {
-            path: '/sw.js',
-          }
+
 
   if('serviceWorker' in navigator){
 navigator.serviceWorker.register('/sw.js')
   .then((reg) => console.log('service worker registered', reg))
   .catch((err) => console.log('service worker not registered', err));
+}
+
+
+  if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
 }
