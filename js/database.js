fad0bd6519bc2d3986f5ce1a37e61e9b767a4d96
IkraT101 <IkraTameel@hotmail.com>
2019-11-30 21:18:54 +0000

    adding a new data base
    new attempt at server register

--- js/database.js
+++ js/database.js
@@ -1,6 +1,5 @@
-if('serviceworker' in navegaitor){
-navegaitor.serviceworker.register(/sw.js)
+if('serviceWorker' in navigator){
+navigator.serviceWorker.register('/sw.js')
   .then((reg) => console.log('service worker registered', reg))
-  .catch((err) => console.log('service worker not registered', err))
-
+  .catch((err) => console.log('service worker not registered', err));
 }
